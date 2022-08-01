62cf5073fa03f848547117b0e374179f555a3826
MD. Nazmul Hasan Anik <anikology@gmail.com>
2022-08-01 15:04:47 +0600

    route updation

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('register',[StudentController::class,""]);
+Route::post('login',[StudentController::class,""]);
+
+Route::group(["middleware"=>["auth:sanctum"]],function(){
+    Route::get('profile',[StudentController::class,""]);
+    Route::get('logout',[StudentController::class,""]);
+
+    // Project 
+    Route::post('create',[ProjectController::class,""]);
+    Route::get('list',[ProjectController::class,""]);
+    Route::get('single/{$id}',[ProjectController::class,""]);
+    Route::delete('delete/{$id}',[ProjectController::class,""]);
+});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
