fdead375ce5c90d585d23f66560638a13c1074ae
MD. Nazmul Hasan Anik <anikology@gmail.com>
2022-08-01 14:43:25 +0600

    Controller Setting

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    
+    public function create(Request $request){
+
+    }
+    public function list(){
+
+    }
+    public function single($id){
+
+    }
+    public function delete($id){
+
+    }
+}
---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    //
+    public function register(Request $request){
+
+    }
+    public function login(Request $request){
+        
+    }
+    public function profile(){
+        
+    }
+    public function logout(){
+        
+    }
+
+}
